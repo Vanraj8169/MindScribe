6229cb9351a065158cb0e350ff48f4a61598e78f
Vanraj Pardeshi <truecapturers@gmail.com>
2024-10-31 18:18:35 +0530

    Search Functionality implemented

--- app/dashboard/_components/TemplateListSection.tsx
+++ app/dashboard/_components/TemplateListSection.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
 
-const TemplateListSection = () => {
-  return (
-    <div>
-      template list section
-    </div>
-  )
+import Templates from "@/app/(data)/Templates";
+import React, { useEffect, useState } from "react";
+import TemplateCard from "./TemplateCard";
+
+export interface Template {
+  name: string;
+  desc: string;
+  icon: string;
+  category: string;
+  slug: string;
+  aiPrompt: string;
+  form?: Form[];
 }
 
-export default TemplateListSection
+export interface Form {
+  label: string;
+  field: string;
+  name: string;
+  required?: boolean;
+}
+const TemplateListSection = ({ userSearchInput }: any) => {
+  const [templateList, setTemplateList] = useState(Templates);
+  useEffect(() => {
+    if (userSearchInput) {
+      const filteredData = Templates.filter((template) =>
+        template.name.toLowerCase().includes(userSearchInput.toLowerCase())
+      );
+      setTemplateList(filteredData);
+    } else {
+        setTemplateList(Templates);
+    }
+  }, [userSearchInput]);
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-10">
+      {templateList.map((template: Template, index: number) => (
+        <TemplateCard {...template} key={index}/>
+      ))}
+    </div>
+  );
+};
+
+export default TemplateListSection;
